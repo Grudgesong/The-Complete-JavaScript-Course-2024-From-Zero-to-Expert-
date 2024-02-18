02a55098f9184291449e8370b0f64eff767aa370
Sangraj Ranjit <Grudgesong@gmail.com>
2024-02-18 20:31:54 +0800

    Handling click events

--- 1_Guess My Nomber/script.js
+++ 1_Guess My Nomber/script.js
@@ -4,3 +4,11 @@ document.querySelector('.message');
 document.querySelector('.number');
 document.querySelector('.score');
 document.querySelector('.guess');
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  !guess
+    ? (document.querySelector('.message').textContent = 'No Number!!')
+    : '';
+});
