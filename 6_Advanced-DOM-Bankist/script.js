350596053fcec598638196ccb390e34d69cc0d16
Sangraj Ranjit <Grudgesong@gmail.com>
2024-03-02 13:18:18 +0800

    DOM transversing

--- 6_Advanced-DOM-Bankist/script.js
+++ 6_Advanced-DOM-Bankist/script.js
@@ -90,6 +90,38 @@ btnScrollTo.addEventListener('click', function (e) {
   section1.scrollIntoView({ behavior: 'smooth' });
 });
 
+//DOM Transversing
+/*
+const h1= document.querySelector('h1');
+
+//Going downwards child
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children);// works only for direct children
+h1.firstElementChild.computedStyleMap.color= 'white';
+h1.lastElementChild.computedStyleMap.color= 'orangered';
+
+
+//Going upwards
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest('.header').color.background='var(--gradient-secondary)';
+h1.closest('h1').color.background='var(--gradient-primary)';
+
+//Going sideways siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(function(el){
+  if(el!==h1) el.style.transform= 'scale(0.5)';
+});
+*/
+
 //------------------------------LECTURE---------------------------------------
 /*
 ///////////////////////////////////////
