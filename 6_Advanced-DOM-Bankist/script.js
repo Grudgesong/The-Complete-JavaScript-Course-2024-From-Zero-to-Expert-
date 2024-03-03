7a1174b0f78d0983e55c7fc40dd1045ce562a979
Sangraj Ranjit <Grudgesong@gmail.com>
2024-03-03 22:27:35 +0800

    Sticky Navigation- Scroll event

--- 6_Advanced-DOM-Bankist/script.js
+++ 6_Advanced-DOM-Bankist/script.js
@@ -104,6 +104,15 @@ const handleHover = function (e) {
 nav.addEventListener('mouseover', handleHover.bind(0.5));
 nav.addEventListener('mouseout', handleHover.bind(1));
 
+//Sticky Navigation----------Scroll Event--------------
+//Not very efficient
+const initialCoords = section1.getBoundingClientRect();
+window.addEventListener('scroll', function (e) {
+  // console.log(window.scrollY);
+  if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+});
+
 //Implementing Smooth scrolling
 
 btnScrollTo.addEventListener('click', function (e) {
