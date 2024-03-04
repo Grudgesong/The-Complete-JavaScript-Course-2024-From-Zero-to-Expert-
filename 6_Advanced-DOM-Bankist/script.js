75f2c9c9d1c95f7e177f68c757dd4e359002ea9b
Sangraj Ranjit <Grudgesong@gmail.com>
2024-03-04 12:43:47 +0800

    Revealing Elements on scroll- Intersection Observer API

--- 6_Advanced-DOM-Bankist/script.js
+++ 6_Advanced-DOM-Bankist/script.js
@@ -150,6 +150,26 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 });
 headerObserver.observe(header);
 
+//Revealing elements on scroll---------------------------------------
+const allSections = document.querySelectorAll('.section');
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
+
 //Implementing Smooth scrolling
 
 btnScrollTo.addEventListener('click', function (e) {
