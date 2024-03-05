7f5deb72775ea4b1aea14e36ce4ebfdea747e69a
Sangraj Ranjit <Grudgesong@gmail.com>
2024-03-05 12:41:40 +0800

    Creating a new workout

--- 7_Mapty App-OOP,Geolocation,External Libraries and More/script.js
+++ 7_Mapty App-OOP,Geolocation,External Libraries and More/script.js
@@ -16,7 +16,7 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 //Managing workout data
 class Workout {
-  date = new this.date();
+  date = new Date();
   id = (Date.now() + '').slice(-10);
   constructor(coords, distance, duration) {
     this.coords = coords; // [lat, lng]
@@ -26,6 +26,8 @@ class Workout {
 }
 
 class Running extends Workout {
+  type = 'running';
+
   constructor(coords, distance, duration, cadence) {
     super(coords, distance, duration);
     this.cadence = cadence;
@@ -40,6 +42,7 @@ class Running extends Workout {
 }
 
 class Cycling extends Workout {
+  type = 'cycling';
   constructor(coords, distance, duration, elevationGain) {
     super(coords, distance, duration);
     this.elevationGain = elevationGain;
@@ -59,6 +62,7 @@ class App {
   //private
   #map;
   #mapEvent;
+  #workouts = [];
 
   constructor() {
     this._getPosition();
@@ -114,19 +118,76 @@ class App {
   }
 
   _newWorkout(e) {
+    //Helper Functions
+    const validInputs = (...inputs) =>
+      inputs.every(inp => Number.isFinite(inp));
+
+    /*...inputs: This collects all arguments passed to the function into an array named 
+      inputs.
+
+      inputs.every(...): This method tests whether all elements in the array pass the test 
+      implemented by the provided function. It returns a Boolean value (true or false).
+
+      inp => Number.isFinite(inp): For each element (inp) in the array, this checks if it is a 
+      finite number using Number.isFinite. This method returns true if the argument is a 
+      finite number, and false otherwise (including Infinity, -Infinity, NaN, or non-numeric
+      values). */
+
+    const allPositive = (...inputs) => inputs.every(inp => inp > 0);
     // Rendering Workout input form
     e.preventDefault();
 
+    //Get data from form
+    const type = inputType.value;
+    const distance = +inputDistance.value; //+ converts string to number
+    const duration = +inputDuration.value;
+    const { lat, lng } = this.#mapEvent.latlng;
+    let workout;
+    //Check if data is valid
+    //If running, create running object
+    if (type === 'running') {
+      const cadence = +inputCadence.value;
+      //check if data is valid
+      if (
+        // !Number.isFinite(distance) ||
+        // !Number.isFinite(duration) ||
+        // !Number.isFinite(cadence)
+        !validInputs(distance, duration, cadence) ||
+        !allPositive(distance, duration, cadence)
+      )
+        return alert('Input has to be positive numbers!!');
+
+      workout = new Running([lat, lng], distance, duration, cadence);
+    }
+    //If cycling, create cycling object
+    if (type === 'cycling') {
+      const elevation = +inputElevation.value;
+      //check if data is valid
+      if (
+        !validInputs(distance, duration, elevation) ||
+        !allPositive(distance, duration)
+      )
+        return alert('Input has to be positive numbers!!');
+      workout = new Cycling([lat, lng], distance, duration, elevation);
+    }
+    //Add new object to working array
+    this.#workouts.push(workout);
+    //render workout on map as marker
+    this.renderWorkoutMarker(workout);
+    //Render workout on list
+    //Hiide form + clear input fields
     //Clear input fields
     inputDistance.value =
       inputDuration.value =
       inputCadence.value =
       inputElevation.value =
         '';
+  }
 
+  renderWorkoutMarker(workout) {
     //Displaying a Map marker
-    const { lat, lng } = this.#mapEvent.latlng;
-    L.marker({ lat, lng })
+
+    L.marker(workout.coords)
       .addTo(this.#map)
       .bindPopup(
         L.popup({
@@ -134,7 +195,7 @@ class App {
           minWidth: 100,
           autoClose: false,
           closeOnClick: false,
-          className: 'running-popup',
+          className: `${workout.type}-popup`,
         })
       )
       .setPopupContent('Workout')
